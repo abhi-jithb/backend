8f3634515efae388f32cb0bd751e033722cf00bd
abhi-jithb <babhijith58@gmail.com>
2025-08-29 17:55:19 +0530

    README.md file added based on todays learnings

---
+++ README.d
@@ -0,0 +1,11 @@
+# HAI I'm learning backend from today (29/08/2025) at TinkerSpace
+
+# Day 1
+* I learned basics of backend from 1pm to 6pm 
+* Topics that i learned & dived into are: 
+> how to run a simple server
+> routing
+> env (development, production, user)
+> learned testing on postman
+
+#Day 2
