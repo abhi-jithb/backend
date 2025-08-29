0f1245136c3190b91de3493fe1dc20e8fa9cf486
abhi-jithb <babhijith58@gmail.com>
2025-08-29 16:21:08 +0530

    browser  view tested

--- app.js
+++ app.js
@@ -1,56 +1,39 @@
-// import express from 'express';
-
-// const app = express();
-
-// app.get('/', (req, res) =>{
-//     res.send({ body: 'Welcome to TinkerSpace..'});
-// });
-
-// app.listen(3000, () => {
-//     console.log("Server running at TinkerSpace on port 3000!!")
-// })
-
-// export default app;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-import express from "express";
+import express from 'express';
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({body: "aree yaar welcome to TS!"});
+app.get('/', (req, res) =>{
+    res.send('Welcome to TinkerSpace..');
 });
 
-app.listen(4000, () => {
-    console.log("Server is running at tinker space!")
+app.listen(3000, () => {
+    console.log("Server running at TinkerSpace on http://localhost:3000 ")
 })
 
 export default app;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
